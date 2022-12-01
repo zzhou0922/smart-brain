97b7e45e2b5f71b688653e9cd87c155c48a16119
Zebin Zhou <zb.zhou0922@gmail.com>
2022-12-01 14:34:39 -0800

    Correcting spelling for navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,13 +5,13 @@ const Navigation = ( {onRouteChange, isSignedIn} ) => {
 	if(isSignedIn) {
 		return (
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-				<p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'> Sing Out </p>
+				<p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'> Sign Out </p>
 			</nav>
 		);
 	} else {
 		return (
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-				<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Sing In </p>
+				<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Sign In </p>
 				<p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register </p>
 			</nav>
 		);
